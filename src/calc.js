508d5bcffe01b8ec8855a32cc66969f774cfd511
Totoshk0 <Fatkulinsasha@gmail.com>
2024-02-07 14:44:45 +0300

    Test_6V002

--- src/calc.js
+++ src/calc.js
@@ -1,39 +1,37 @@
 import readlineSync from 'readline-sync';
 import { greetUser } from './cli.js';
 
+const generateExpression = () => {
+  const num1 = Math.floor(Math.random() * 100) + 1;
+  const num2 = Math.floor(Math.random() * 100) + 1;
+  const operators = ['+', '-', '*'];
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  const expression = `${num1} ${operator} ${num2}`;
+  return expression;
+};
+
+const resultExpression = (expression) => eval(expression);
+
 export const calc = () => {
+  const userName = greetUser();
 
-    const userName = greetUser();
+  let correctAnswersCount = 0;
+  console.log('What is the result of the expression ?');
+  while (correctAnswersCount < 3) {
+    const str = generateExpression();
 
-    let correctAnswersCount = 0;
-    console.log('What is the result of the expression ?');
-    while (correctAnswersCount < 3) {
-        const generateExpression = () => {
+    console.log(`Question: ${str}`);
 
-            const num1 = Math.floor(Math.random() * 100) + 1;
-            const num2 = Math.floor(Math.random() * 100) + 1;
-            const operators = ['+', '-', '*'];
-            const operator = operators[Math.floor(Math.random() * operators.length)];
-            const expression = `${num1} ${operator} ${num2}`;
-            return expression;
-        };
-        const resultExpression = (expression) => {
-            return eval(expression);
-        };
-        const str = generateExpression()
+    const userAnwser = readlineSync.question('Your answer: ');
 
-        console.log(`Question: ${str}`);
-
-        const userAnwser = readlineSync.question('Your answer: ');
-
-        if (Number(userAnwser) === resultExpression(str)) {
-            console.log("Correct!");
-            correctAnswersCount++;
-        } else {
-            console.log(`'${userAnwser}' is wrong answer ;(. Correct answer was '${resultExpression(str)}'.`);
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        }
+    if (Number(userAnwser) === resultExpression(str)) {
+      console.log('Correct!');
+      correctAnswersCount++;
+    } else {
+      console.log(`'${userAnwser}' is wrong answer ;(. Correct answer was '${resultExpression(str)}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
-    console.log(`Congratulations, ${userName}!`);
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
--- src/random_Num.js
+++ src/random_Num.js
@@ -1,33 +1,30 @@
 import readlineSync from 'readline-sync';
 import { greetUser } from './cli.js';
 
-const isEven = (number) => {
-    return number % 2 === 0;
-};
+const isEven = (number) => number % 2 === 0;
 
 export const randomNum = () => {
+  const userName = greetUser();
 
-    const userName = greetUser();
+  console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
-    console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
+  let correctAnswersCount = 0;
+  while (correctAnswersCount < 3) {
+    const questionNumber = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = isEven(questionNumber) ? 'yes' : 'no';
 
-    let correctAnswersCount = 0;
-    while (correctAnswersCount < 3) {
-        const questionNumber = Math.floor(Math.random() * 100) + 1;
-        const correctAnswer = isEven(questionNumber) ? 'yes' : 'no';
+    console.log(`Question: ${questionNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-        console.log(`Question: ${questionNumber}`);
-        const userAnswer = readlineSync.question("Your answer: ");
-
-        if (userAnswer.toLowerCase() === correctAnswer) {
-            console.log("Correct!");
-            correctAnswersCount++;
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        }
+    if (userAnswer.toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount++;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+  }
 
-    console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
